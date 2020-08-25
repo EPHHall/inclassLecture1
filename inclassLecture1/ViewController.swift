c5550f49c497b1c63a7f435495486bf10010c257
Ethan Hall <ephall00@gmail.com>
2020-08-25 13:03:40 -0400

    Initial Commit

---
+++ inclassLecture1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inclassLecture1
+//
+//  Created by  on 8/25/20.
+//  Copyright © 2020 CS485GFall2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
